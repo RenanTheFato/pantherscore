32649e41bbaec5aa73c045251c9d4d964f929c69
RenanTheFato <renan.santana007@hotmail.com>
2026-04-24 16:35:47 -0300

    add env parsing validation with zod

---
+++ src/config/env.ts
@@ -0,0 +1,11 @@
+import { z } from "zod/v4";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  HOST: z.coerce.string().default("0.0.0.0")
+})
+
+export const env = envSchema.parse(process.env)
